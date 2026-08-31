69c43ea569f9dd6f7b3e2a2087d69e6d1e953e47
SudarsanGouda-Adobe <inf16883@adobe.com>
2026-08-31 15:36:31 +0530

    product css chagnes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,39 +1,10 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default function decorate(block) {debugger;
   const img = block.querySelector('img');
   const heading = block.querySelector('h1');
   const description = block.querySelectorAll('p')[0];
   const cta = block.querySelector('a');
-
-
-
-  const img1= block.querySelector('img');
-  const heading1=block.querySelector('h1');
-  const desc_=block.querySelectorAll('p')[0];
-  const banenr_ = document.createElement('div');
-  banenr_.className='banner';
-  banenr_.innerHTML=`<div class="banner-media">
-        <div class="banner-content"></div>
-    </div>`
-
-
-const media_=banenr_.querySelector('.banner-media');
-const content_=banenr_.querySelector('.banner-content');
-if(img1){
-    media_.append(createOptimizedPicture(
-        img.src,
-        img.alt,
-        false,
-        [{width:'750px'},{width:'1200px'},{with:'2000px'}]
-    ))
-}
-
-
-
-
-
-
 
   const banner = document.createElement('div');
   banner.className = 'banner';
