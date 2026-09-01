e5c990b6263e557758a484152922bc92d3db715f
SudarsanGouda-Adobe <inf16883@adobe.com>
2026-09-01 15:52:26 +0530

    embaded twitter js changd

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,7 +1,11 @@
+
 export default function decorate(block) {
   const link = block.querySelector('a');
 
-  if (!link) return;
+  if (!link) {
+    console.log('Embed: No link found');
+    return;
+  }
 
   const url = link.href;
 
@@ -14,10 +18,17 @@ export default function decorate(block) {
 
       if (parsedUrl.hostname.includes('youtu.be')) {
         videoId = parsedUrl.pathname.substring(1);
+      } else if (parsedUrl.pathname.includes('/shorts/')) {
+        videoId = parsedUrl.pathname.split('/shorts/')[1];
+      } else if (parsedUrl.pathname.includes('/embed/')) {
+        videoId = parsedUrl.pathname.split('/embed/')[1];
       } else {
         videoId = parsedUrl.searchParams.get('v');
       }
+
+      videoId = videoId?.split(/[?&#]/)[0];
     } catch (error) {
+      console.error('Invalid YouTube URL:', error);
       return;
     }
 
@@ -45,25 +56,41 @@ export default function decorate(block) {
     blockquote.className = 'twitter-tweet';
 
     const tweetLink = document.createElement('a');
-
     tweetLink.href = url;
+    tweetLink.target = '_blank';
+    tweetLink.rel = 'noopener noreferrer';
+    tweetLink.textContent = 'View this post on X';
 
     blockquote.append(tweetLink);
 
     block.innerHTML = '';
     block.append(blockquote);
 
-    const script = document.createElement('script');
-
-    script.src = 'https://platform.twitter.com/widgets.js';
-    script.async = true;
-
-    script.onload = () => {
-      if (window.twttr) {
+    const loadTwitterWidget = () => {
+      if (window.twttr && window.twttr.widgets) {
         window.twttr.widgets.load(block);
       }
     };
 
-    document.body.append(script);
+    if (window.twttr && window.twttr.widgets) {
+      loadTwitterWidget();
+    } else {
+      const existingScript = document.querySelector(
+        'script[src="https://platform.twitter.com/widgets.js"]',
+      );
+
+      if (existingScript) {
+        existingScript.addEventListener('load', loadTwitterWidget);
+      } else {
+        const script = document.createElement('script');
+
+        script.src = 'https://platform.twitter.com/widgets.js';
+        script.async = true;
+
+        script.onload = loadTwitterWidget;
+
+        document.body.append(script);
+      }
+    }
   }
 }
